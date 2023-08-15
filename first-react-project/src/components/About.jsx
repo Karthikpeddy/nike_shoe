ec434957254dd00f51a5df47d85a10aa630bef84
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-15 18:08:29 +0530

    first_commit

---
+++ first-react-project/src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="about">
+      <div className="heading">
+        <h1>About Us</h1>
+      </div>
+    </div>
+  );
+};
+export default About;
