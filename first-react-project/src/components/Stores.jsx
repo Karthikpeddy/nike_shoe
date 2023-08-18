8155c48e7d0d1a0ba4a5736ff559064cf0ab22b0
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-19 00:12:33 +0530

    stores_changes_0.0.1

--- first-react-project/src/components/Stores.jsx
+++ first-react-project/src/components/Stores.jsx
@@ -1,4 +1,16 @@
 const Stores = () => {
-  return <h1>Stores</h1>;
+  return (
+    <div className="stores">
+      <div className="stores-content">
+        <div className="stores-heading">
+          <h1>Stores</h1>
+        </div>
+        <div className="stores-pic">
+          <img src="./store_pic.jpeg" alt="stores-pic" />
+        </div>
+        <div className="stores-matter"></div>
+      </div>
+    </div>
+  );
 };
 export default Stores;
