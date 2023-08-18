5a7c0b7f4fde25279ccb3e6a8075b2677095e9cd
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-18 22:15:52 +0530

    about-final

--- first-react-project/src/components/About.jsx
+++ first-react-project/src/components/About.jsx
@@ -1,16 +1,20 @@
 const About = () => {
   return (
     <div className="about">
-      <div className="about-heading">
-        <h1>About Us</h1>
-      </div>
-      <div className="about-matter">
-        <p>
-          We champion continual progress for athletes and sport by taking action
-          to help athletes reach their potential. Every job at NIKE, Inc. is
-          grounded in a team-first mindset, cultivating a culture of innovation
-          and a shared purpose to leave an enduring impact.
-        </p>
+      <div className="about-content">
+        <div className="about-matter">
+          <h1>About Us</h1>{" "}
+          <p>
+            We champion continual progress for athletes and sport by taking
+            action to help athletes reach their potential. Every job at NIKE,
+            Inc. is grounded in a team-first mindset, cultivating a culture of
+            innovation and a shared purpose to leave an enduring impact.
+          </p>
+        </div>
+
+        <div className="about-image">
+          <img src="./about_pic_01.png" alt="about" />
+        </div>
       </div>
     </div>
   );
