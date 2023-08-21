12e348fa939476da340ef72591bcd56ea2647c76
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-21 18:03:06 +0530

    contact_changes_0.0.1

--- first-react-project/src/components/home.jsx
+++ first-react-project/src/components/home.jsx
@@ -1,4 +1,11 @@
+import React, { useState } from "react";
 const Home = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    console.log(isDropdownOpen);
+    setDropdownOpen(!isDropdownOpen);
+  };
   return (
     <main className="home bg-white">
       <div className="home-content">
@@ -11,10 +18,78 @@ const Home = () => {
         <br />
         <div className="home-button">
           <div>
-            <button className="shopnow rounded-full">SHOP NOW</button>
+            <button className="shopnow ">SHOP NOW</button>
           </div>
-          <div>
-            <button className="category rounded-full">Category</button>
+          <div class="flex">
+            <button
+              id="dropdown-button"
+              data-dropdown-toggle="dropdown"
+              class="z-10 inline-flex flex-shrink-0 items-center rounded-lg border border-gray-500 bg-gray-50 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              type="button"
+              onClick={toggleDropdown}
+            >
+              Categories
+              <svg
+                class="ml-2.5 h-2.5 w-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+            {isDropdownOpen && (
+              <div
+                id="dropdown"
+                class="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+              >
+                <ul
+                  class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                  aria-labelledby="dropdown-button"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Shopping
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Images
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      News
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Finance
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+            ;
           </div>
         </div>
         <br />
