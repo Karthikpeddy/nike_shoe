be307239e6482b312f48dd0f875f35ea110d3076
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-21 22:57:16 +0530

    changes

--- first-react-project/src/components/home.jsx
+++ first-react-project/src/components/home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 const Home = () => {
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isDropdownOpen, setisDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
     console.log(isDropdownOpen);
@@ -23,17 +23,17 @@ const Home = () => {
           <div>
             <button className="shopnow ">SHOP NOW</button>
           </div>
-          <div class="flex">
+          <div className="flex">
             <button
               id="dropdown-button"
               data-dropdown-toggle="dropdown"
-              class="z-10 inline-flex flex-shrink-0 items-center rounded-lg border border-gray-500 bg-gray-50 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              className="z-10 inline-flex flex-shrink-0 items-center rounded-lg border border-gray-500 bg-gray-50 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
               type="button"
-              onClick={() => setDropdownOpen((prev) => !prev)}
+              onClick={() => setisDropdownOpen((prev) => !prev)}
             >
               Categories
               <svg
-                class="ml-2.5 h-2.5 w-2.5"
+                className="ml-2.5 h-2.5 w-2.5"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -51,16 +51,16 @@ const Home = () => {
             {isDropdownOpen && (
               <div
                 id="dropdown"
-                class="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+                className="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
               >
                 <ul
-                  class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                  className="py-2 text-sm text-gray-700 dark:text-gray-200"
                   aria-labelledby="dropdown-button"
                 >
                   <li>
                     <a
                       href="#"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       New & Featured
                     </a>
@@ -68,7 +68,7 @@ const Home = () => {
                   <li>
                     <a
                       href="#"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       Men
                     </a>
@@ -76,7 +76,7 @@ const Home = () => {
                   <li>
                     <a
                       href="#"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       Women
                     </a>
@@ -84,7 +84,7 @@ const Home = () => {
                   <li>
                     <a
                       href="#"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       Kids
                     </a>
