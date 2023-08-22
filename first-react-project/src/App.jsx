585a25ec568b03f2a43b98f25e4df07c3f9ff834
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-22 23:07:19 +0530

    routes_0.0.2

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -4,10 +4,15 @@ import Contact from "./components/Contact";
 import Stores from "./components/Stores";
 import Home from "./components/home";
 import Navbar from "./components/navbar";
+import { Routes, Route } from "react-router-dom";
+import Login01 from "./components/Login01";
 
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route path="/about" element={<Login01 />} />
+      </Routes>
       <Navbar />
       <Home />
       <About />
