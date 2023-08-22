261acfc532771d688d891421b8b82472867ea958
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-23 01:30:26 +0530

    navbar_changes

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Navbar />
       <Home />
