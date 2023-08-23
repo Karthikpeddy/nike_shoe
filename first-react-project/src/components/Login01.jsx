e2eca0b23469f46fa7f504f0dc1c05ad40e85871
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-23 22:05:53 +0530

    login_0.0.1

--- first-react-project/src/components/Login01.jsx
+++ first-react-project/src/components/Login01.jsx
@@ -18,6 +18,8 @@ const Login = () => {
             <input
               type="text"
               placeholder="Username"
+              pattern="[a-z0-9._%+\-]"
+              title=""
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
@@ -27,12 +29,18 @@ const Login = () => {
               type="password"
               placeholder="Password"
               value={password}
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button className="login-button" onClick={handleLogin}>
             Log In
           </button>
+          <br />
+          <br />
+          <p className="mb-3 text-gray-500 ">Not Registered ?</p>
+          <link to></link>
         </form>
       </div>
     </div>
