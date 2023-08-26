cdd7fc0cf0b544daabd68c52a1f1dcd17728a0f9
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-26 17:42:16 +0530

    changes_0.1

--- first-react-project/src/components/Login01.jsx
+++ first-react-project/src/components/Login01.jsx
@@ -26,6 +26,7 @@ const Login = () => {
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
+                required
               />
             </div>
             <div className="input-container">
@@ -37,6 +38,7 @@ const Login = () => {
                 title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 onChange={(e) => setPassword(e.target.value)}
+                required
               />
             </div>
 
--- first-react-project/src/components/contact_s.jsx
+++ first-react-project/src/components/contact_s.jsx
@@ -1,7 +1,14 @@
 const Contact_s = () => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md">
-      <h1 className="text-lg  text-center">Thanks for Contacting us </h1>
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-white p-6 rounded-lg shadow-md">
+        <h1 className="text-lg  text-center">Thanks for Contacting us </h1>
+        <p>
+          We will be in touch with you in 24 to 36 hours. Meanwhile you can
+          email us at "example@gmail.com"
+        </p>
+        <a mailto="peddykarthik1027@gmail.com">peddykarthik1027@gmail.com</a>
+      </div>
     </div>
   );
 };
