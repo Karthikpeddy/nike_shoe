af8f05f1b0e301bcb32fd2ba70779b6c43be5d5d
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-27 11:39:24 +0530

    datepicker

--- first-react-project/src/components/signup.jsx
+++ first-react-project/src/components/signup.jsx
@@ -53,7 +53,10 @@ const Signup = () => {
                     placeholderText="Date of Birth"
                     dateFormat="dd/MM/yyyy"
                     maxDate={new Date()}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    showYearDropdown
+                    scrollableMonthYearDropdown
+                    isClearable
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  transition-colors duration-300 ease-in-out dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
                   />
                 </div>
