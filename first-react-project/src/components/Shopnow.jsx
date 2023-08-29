708c5470a92685f94d048e1637e29ab303eb5d98
Karthikpeddy <peddykarthik1027@gmail.com>
2023-08-29 21:06:54 +0530

    shopnow_0.0.1

--- first-react-project/src/components/Shopnow.jsx
+++ first-react-project/src/components/Shopnow.jsx
@@ -1,7 +1,9 @@
 const Shopnow = () => {
   return (
     <div>
-      <h1 className="text-xl font-bold text-center">Nike products</h1>
+      <div>
+        <h1 className="text-xl font-bold text-center">Nike products</h1>
+      </div>
     </div>
   );
 };
